11a380a406aa66ff3ae10a4abf1b465c6924bbdb
Kat <105393029+surfergrl@users.noreply.github.com>
2024-12-28 18:32:25 +0000

    fixing page links

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -12,9 +13,16 @@ def index():
 def about():
     return render_template("about.html")
 
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
 
 if __name__ == "__main__":
     app.run(
