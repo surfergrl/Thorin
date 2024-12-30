87f830f2a17f9ddc4dd93582f82287c81236d80c
Kat <105393029+surfergrl@users.noreply.github.com>
2024-12-30 19:17:48 +0000

    Adding backend functionality to contact form using POST method

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os 
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,9 +9,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-"""
-following is for the main about page 
-"""
+
 @app.route("/about")
 def about():
     data = []
@@ -29,10 +27,13 @@ def about_member(member_name):
             if obj["url"] == member_name:
                 member = obj
     return render_template("member.html", member=member)
-              
 
-@app.route("/contact")
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print(request.form.get("name"))
+        print(request.form["email"])
     return render_template("contact.html", page_title="Contact")
 
 
