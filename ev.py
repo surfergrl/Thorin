13610d90ca6c9739c6a980000394887f778d87c3
Kat <105393029+surfergrl@users.noreply.github.com>
2025-01-07 17:30:03 +0000

    Adding flash messages for user feedback

--- ev.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.environ.setdefault("SECRET_KEY", "secret_flash_key_")
--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import os 
 import json
 from flask import Flask, render_template, request, flash 
-if os.file.exist(env.py)
+
+if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
